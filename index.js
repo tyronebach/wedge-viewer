a99df5c25c052e20acc7e99f99e434e35d1faa14
bink <binktogether@gmail.com>
2025-04-18 01:24:59 -0700

    open port

--- index.js
+++ index.js
@@ -138,6 +138,6 @@ app.get('/api/imageMeta', async (req, res) => {
 	}
 });
 
-app.listen(port, () => {
-	console.log(`Server running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+	console.log(`Server running on http://0.0.0.0:${port}`);
 });
